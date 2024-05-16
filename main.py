d7ce7366e8c31685ec6c9b34851c83d850906c25
RiccardoCaruso <38222340+RVJIM@users.noreply.github.com>
2024-05-16 11:21:17 +0200

    altra pulizia
    devo sistemare la parte di valuta che ci sono parole non riguardanti le valute

--- main.py
+++ main.py
@@ -7,24 +7,34 @@ from tabulate import tabulate
 table = pd.read_excel("Ask A Manager Salary Survey 2021 (Responses).xlsx")
 df = pd.DataFrame(table)
 
+<<<<<<< Updated upstream
 
 '''# Delete columns that we don't use
 df = df.drop([0, 4, 9, 11, 12], axis = 1)
+=======
+# Delete na values of last 3 columns
+df = df.dropna(subset=df.iloc[:, -3:].columns)
+df = df.dropna(subset=df.columns[2])
+df.iloc[:, 3] = df.iloc[:, 3].replace('na', np.nan)
+df = df.dropna(subset=df.columns[3])
+>>>>>>> Stashed changes
 
-print(df.columns)
-df.loc[df['If "Other," please indicate the currency here: '] == 'Other', 'A'] = df.loc[df['A'] == 'Other', 'B']
+# Delete columns that we don't use
+columns_to_delete = ['Timestamp', 'If your job title needs additional context, please clarify here:',
+                        'If your income needs additional context, please provide it here:', 
+                        "If you're in the U.S., what state do you work in?", 'What city do you work in?', 
+                        'How many years of professional work experience do you have overall?']
+df = df.drop(columns=columns_to_delete)
 
-names_columns = ['Age', 'Industry', 'Job Title', 'Salary', 'Additional', 'Currency', 'Country']
-df_columns = names_columns
-#list_columns = df.columns.values.tolist()
-#print(list_columns)
+# Change columns' name
+names_columns = ['Age', 'Industry', 'Job Title', 'Salary', 'Additional', 'Currency', 'Other Currency', 'Country', 'Years job',
+                 'Highest education', 'Gender', 'Race']
+df.columns = names_columns
 
-
-'''
 # Delete elements where the salary is equal to 0 
-df = df[df.iloc[:, 4] != 0]
+df = df[df.iloc[:, 3] != 0]
 
-df['What country do you work in?'] = df['What country do you work in?'].str.strip()
+df['Country'] = df['Country'].str.strip()
 
 # Filtered by USA
 us = ['U.S', 'U.S>', 'United States', 'usa', 'United States of America', 'United States Of America', 'UXZ',
@@ -44,24 +54,11 @@ us = ['U.S', 'U.S>', 'United States', 'usa', 'United States of America', 'United
         'United States (I work from home and my clients are all over the US/Canada/PR', 'For the United States government, but posted overseas',
         'Currently finance', 'bonus based on meeting yearly goals set w/ my supervisor', 'Y', 'US govt employee overseas, country withheld',
         "I earn commission on sales. If I meet quota, I'm guaranteed another 16k min. Last year i earned an additional 27k. It's not uncommon for people in my space to earn 100k+ after commission.",
-        'I work for a UAE-based organization, though I am personally in the US.', "USA, but for foreign gov't", 'Remote',
-        'Cayman Islands', 'The Bahamas', 'Bermuda']
-df['What country do you work in?'] = df['What country do you work in?'].replace(us, 'USA')
+        'I work for a UAE-based organization, though I am personally in the US.', "USA, but for foreign gov't", 'Remote']
 
-# Filtered by Europe
-europe = ['europe','FRANCE', 'Jersey, Channel islands', 'the netherlands']
-
-europe = {'the Netherlands': 'The Netherlands', 'NL': 'The Netherlands', 'Danmark': 'Denmark', 'Czechia': 'Czech Republic',
-                'Czech republic': 'Czech Republic', 'Company in Germany. I work from Pakistan.': 'Germany', 'croatia': 'Croatia',
-                'finland':'Finland', 'france': 'France', 'czech republic': 'Czech Republic', 'denmark': 'Denmark', 'spain': 'Spain',
-                'From Romania, but for an US based company': 'Romania', 'Austria, but I work remotely for a Dutch/British company': 'Austria',
-                'SWITZERLAND': 'Switzerland', 'switzerland': 'Switzerland', 'Luxemburg': 'Luxembourg', 'The netherlands': 'The Netherlands',
-                'ireland': 'Ireland', 'netherlands': 'The Netherlands', 'germany': 'Germany', 'Netherlands': 'The Netherlands',
-                'Nederland': 'Netherlands', 'Nederland': 'The Netherlands', 'Italy (South)': 'Italy', 'Catalonia': 'Spain'}
 # Filtered by Canada
 canada = ['Canada', 'canada', 'CANADA', 'Canda', 'Canadw', 'Can', 'Canad', 'Canadá', 'Csnada', 'Canada, Ottawa, ontario',
-          'I am located in Canada but I work for a company in the US', '$2,175.84/year is deducted for benefits', 'Policy']
-df['What country do you work in?'] = df['What country do you work in?'].replace(canada, 'Canada')
+          'I am located in Canada but I work for a company in the US', '$2,17.84/year is deducted for benefits', 'Policy']
 
 # Filtered by UK
 uk = ['United Kingdom', 'UK', 'England', 'Uk', 'Scotland', 'United kingdom', 'U.K.', 'united kingdom', 'uk',
@@ -70,17 +67,58 @@ uk = ['United Kingdom', 'UK', 'England', 'Uk', 'Scotland', 'United kingdom', 'U.
                 'London', 'ENGLAND', 'Unites kingdom', 'England/UK', 'United Kingdom.', 'UK (Northern Ireland)', 'United Kindom',
                 'United Kingdomk', 'UK, remote', 'England, UK.', 'Britain', 'Englang', 'Wales (United Kingdom)', 'U.K',
                 'England, Gb', 'U.K. (northern England)', 'Isle of Man', 'UK for U.S. company']
-df['What country do you work in?'] = df['What country do you work in?'].replace(uk, 'United Kingdom')
+df['Country'] = df['Country'].replace(us, 'USA')
+df['Country'] = df['Country'].replace(canada, 'Canada')
+df['Country'] = df['Country'].replace(uk, 'United Kingdom')
 
 errage = {'INDIA': 'India', 'Sri lanka': 'Sri Lanka', 'pakistan':'Pakistan', 'ARGENTINA BUT MY ORG IS IN THAILAND': 'Argentina',
                 'United States- Puerto Rico': 'Puerto Rico', 'México':'Mexico', 'Brasil': 'Brazil', 'NZ': 'New Zealand',
                 'New zealand': 'New Zealand', 'australia': 'Australia', 'Australian': 'Australia', 'New Zealand Aotearoa': 'New Zealand',
-                'Australi': 'Australia', 'Aotearoa New Zealand': 'New zealand', 'new zealand': 'New Zealand',  'japan': 'Japan',
-                'From New Zealand but on projects across APAC': 'New Zealand', 'Remote (philippines)': 'Philippines',
+                'Australi': 'Australia', 'Aotearoa New Zealand': 'New Zealand', 'new zealand': 'New Zealand',  'japan': 'Japan',
+                'From New Zealand but on projects across APAC': 'New Zealand', 'Remote (philippines)': 'Philippines', 'FRANCE': 'France',
                 'singapore': 'Singapore', 'UAE': 'United Arab Emirates', 'Japan, US Gov position': 'Japan','Mainland China': 'China',
-                'hong konh': 'Hong Kong', 'NIGERIA': 'Nigeria', 'South africa': 'South Africa'}
+                'hong konh': 'Hong Kong', 'NIGERIA': 'Nigeria', 'South africa': 'South Africa', 'europe': 'Czech Republic',
+                'the Netherlands': 'The Netherlands', 'NL': 'The Netherlands', 'Danmark': 'Denmark', 'Czechia': 'Czech Republic',
+                'Czech republic': 'Czech Republic', 'Company in Germany. I work from Pakistan.': 'Germany', 'croatia': 'Croatia',
+                'finland':'Finland', 'france': 'France', 'czech republic': 'Czech Republic', 'denmark': 'Denmark', 'spain': 'Spain',
+                'From Romania, but for an US based company': 'Romania', 'Austria, but I work remotely for a Dutch/British company': 'Austria',
+                'SWITZERLAND': 'Switzerland', 'switzerland': 'Switzerland', 'Luxemburg': 'Luxembourg', 'The netherlands': 'The Netherlands',
+                'ireland': 'Ireland', 'netherlands': 'The Netherlands', 'germany': 'Germany', 'Netherlands': 'The Netherlands',
+                'Nederland': 'Netherlands', 'Nederland': 'The Netherlands', 'Italy (South)': 'Italy', 'Catalonia': 'Spain',
+                'Jersey, Channel islands': 'Jersey', 'the netherlands': 'The Netherlands'}
 
-df['What country do you work in?'] = df['What country do you work in?'].replace(errage)
+df['Country'] = df['Country'].replace(errage)
 
-total_values = df['What country do you work in?'].value_counts()
-print(tabulate(total_values.reset_index(), headers=['Country', 'Count'], tablefmt='pretty'))
+countries = [
+        'Afghanistan', 'Argentina', 'Australia', 'Austria', 'Bangladesh', 'Belgium', 'Bermuda', 'Bosnia and Herzegovina', 'Brazil',
+        'Bulgaria', 'Cambodia', 'Canada', 'Cayman Islands', 'Chile', 'China', 'Colombia', 'Congo', 'Costa Rica', "Cote d'Ivoire", 'Croatia',
+        'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Ecuador', 'Eritrea', 'Estonia', 'Finland', 'France', 'Germany', 'Ghana', 'Greece',
+        'Hong Kong', 'Hungary', 'India', 'Indonesia', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kenya',
+        'Kuwait', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malaysia', 'Malta', 'Mexico', 'Morocco', 'New Zealand', 'Nigeria',
+        'Norway', 'Pakistan', 'Panamá', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saudi Arabia',
+        'Serbia', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Somalia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'Sweden',
+        'Switzerland', 'Taiwan', 'Thailand', 'The Bahamas', 'The Netherlands', 'Trinidad and Tobago', 'Turkey', 'USA', 'Uganda', 'Ukraine',
+        'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Vietnam'
+        ]
+
+df = df[df['Country'].isin(countries)]
+
+# Shift value from Other Currency to Currency and delete the first 
+currencies = [
+    'USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD', 'ARS',
+    'NOK', 'MXN', 'SGD', 'HKD', 'KRW', 'TRY', 'RUB', 'INR', 'BRL', 'ZAR',
+    'TWD', 'DKK', 'PLN', 'THB', 'IDR', 'HUF', 'CZK', 'ILS', 'CLP', 'PHP',
+    'AED', 'COP', 'SAR', 'MYR', 'RON', 'NGN', 'EGP', 'ARS', 'PKR', 'KES',
+    'VND', 'IQD', 'CLF', 'BDT', 'DZD', 'KZT', 'QAR', 'PEN', 'UAH', 'MAD'
+]
+
+for i, currency in enumerate(df['Other Currency']):
+        if not pd.isna(currency):
+                print(i, currency)
+                df.loc[i, 'Currency'] = currency
+
+df.to_excel('clean.xlsx')
+
+
+#total_values = df['Country'].value_counts()
+#print(tabulate(total_values.reset_index().sort_values(by='Country'), headers=['Country', 'Count'], tablefmt='pretty'))
