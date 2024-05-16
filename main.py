28354b951e9b42633c197767a7fba16ab5f2b610
RiccardoCaruso <38222340+RVJIM@users.noreply.github.com>
2024-05-16 18:46:43 +0200

    Fino a valute
    devo passare adesso a standardizzare

--- main.py
+++ main.py
@@ -14,10 +14,13 @@ df.iloc[:, 3] = df.iloc[:, 3].replace('na', np.nan)
 df = df.dropna(subset=df.columns[3])
 
 # Delete columns that we don't use
-columns_to_delete = ['Timestamp', 'If your job title needs additional context, please clarify here:',
-                        'If your income needs additional context, please provide it here:', 
-                        "If you're in the U.S., what state do you work in?", 'What city do you work in?', 
-                        'How many years of professional work experience do you have overall?']
+columns_to_delete = [
+                'Timestamp', 'If your job title needs additional context, please clarify here:',
+                'If your income needs additional context, please provide it here:', 
+                "If you're in the U.S., what state do you work in?", 'What city do you work in?', 
+                'How many years of professional work experience do you have overall?'
+                ]
+
 df = df.drop(columns=columns_to_delete)
 
 # Change columns' name
@@ -29,9 +32,11 @@ df.columns = names_columns
 df = df[df.iloc[:, 3] != 0]
 
 df['Country'] = df['Country'].str.strip()
+df['Other Currency'] = df['Other Currency'].str.strip()
 
 # Filtered by USA
-us = ['U.S', 'U.S>', 'United States', 'usa', 'United States of America', 'United States Of America', 'UXZ',
+us = [
+        'U.S', 'U.S>', 'United States', 'usa', 'United States of America', 'United States Of America', 'UXZ',
         'U.S.A.', 'US', 'Usa', 'The United State', 'Usa ', 'UsA', 'United  States', 'U.S.A', 'USaa', 'Unted States',
         'United statew', 'United Sttes', 'Unitied States', 'USAB', 'Uniited States', 'Untied States', 'United Statues',
         'United Statesp', 'Uniteed States', 'USS', 'U.s.a.', 'U.SA', 'united stated', 'United Stattes', 'United Statees',
@@ -48,38 +53,46 @@ us = ['U.S', 'U.S>', 'United States', 'usa', 'United States of America', 'United
         'United States (I work from home and my clients are all over the US/Canada/PR', 'For the United States government, but posted overseas',
         'Currently finance', 'bonus based on meeting yearly goals set w/ my supervisor', 'Y', 'US govt employee overseas, country withheld',
         "I earn commission on sales. If I meet quota, I'm guaranteed another 16k min. Last year i earned an additional 27k. It's not uncommon for people in my space to earn 100k+ after commission.",
-        'I work for a UAE-based organization, though I am personally in the US.', "USA, but for foreign gov't", 'Remote']
+        'I work for a UAE-based organization, though I am personally in the US.', "USA, but for foreign gov't", 'Remote'
+        ]
 
 # Filtered by Canada
-canada = ['Canada', 'canada', 'CANADA', 'Canda', 'Canadw', 'Can', 'Canad', 'Canadá', 'Csnada', 'Canada, Ottawa, ontario',
-          'I am located in Canada but I work for a company in the US', '$2,17.84/year is deducted for benefits', 'Policy']
+canada = [
+        'Canada', 'canada', 'CANADA', 'Canda', 'Canadw', 'Can', 'Canad', 'Canadá', 'Csnada', 'Canada, Ottawa, ontario',
+        'I am located in Canada but I work for a company in the US', '$2,17.84/year is deducted for benefits', 'Policy'
+        ]
 
 # Filtered by UK
-uk = ['United Kingdom', 'UK', 'England', 'Uk', 'Scotland', 'United kingdom', 'U.K.', 'united kingdom', 'uk',
-                'Great Britain', 'England, UK', 'Wales', 'England, United Kingdom', 'Northern Ireland', 'Scotland, UK',
-                'england', 'UK (England)', 'United Kingdom (England)', 'Wales (UK)', 'Wales, UK', 'Northern Ireland, United Kingdom',
-                'London', 'ENGLAND', 'Unites kingdom', 'England/UK', 'United Kingdom.', 'UK (Northern Ireland)', 'United Kindom',
-                'United Kingdomk', 'UK, remote', 'England, UK.', 'Britain', 'Englang', 'Wales (United Kingdom)', 'U.K',
-                'England, Gb', 'U.K. (northern England)', 'Isle of Man', 'UK for U.S. company']
+uk = [
+        'United Kingdom', 'UK', 'England', 'Uk', 'Scotland', 'United kingdom', 'U.K.', 'united kingdom', 'uk',
+        'Great Britain', 'England, UK', 'Wales', 'England, United Kingdom', 'Northern Ireland', 'Scotland, UK',
+        'england', 'UK (England)', 'United Kingdom (England)', 'Wales (UK)', 'Wales, UK', 'Northern Ireland, United Kingdom',
+        'London', 'ENGLAND', 'Unites kingdom', 'England/UK', 'United Kingdom.', 'UK (Northern Ireland)', 'United Kindom',
+        'United Kingdomk', 'UK, remote', 'England, UK.', 'Britain', 'Englang', 'Wales (United Kingdom)', 'U.K',
+        'England, Gb', 'U.K. (northern England)', 'Isle of Man', 'UK for U.S. company'
+        ]
+
 df['Country'] = df['Country'].replace(us, 'USA')
 df['Country'] = df['Country'].replace(canada, 'Canada')
 df['Country'] = df['Country'].replace(uk, 'United Kingdom')
 
-errage = {'INDIA': 'India', 'Sri lanka': 'Sri Lanka', 'pakistan':'Pakistan', 'ARGENTINA BUT MY ORG IS IN THAILAND': 'Argentina',
-                'United States- Puerto Rico': 'Puerto Rico', 'México':'Mexico', 'Brasil': 'Brazil', 'NZ': 'New Zealand',
-                'New zealand': 'New Zealand', 'australia': 'Australia', 'Australian': 'Australia', 'New Zealand Aotearoa': 'New Zealand',
-                'Australi': 'Australia', 'Aotearoa New Zealand': 'New Zealand', 'new zealand': 'New Zealand',  'japan': 'Japan',
-                'From New Zealand but on projects across APAC': 'New Zealand', 'Remote (philippines)': 'Philippines', 'FRANCE': 'France',
-                'singapore': 'Singapore', 'UAE': 'United Arab Emirates', 'Japan, US Gov position': 'Japan','Mainland China': 'China',
-                'hong konh': 'Hong Kong', 'NIGERIA': 'Nigeria', 'South africa': 'South Africa', 'europe': 'Czech Republic',
-                'the Netherlands': 'The Netherlands', 'NL': 'The Netherlands', 'Danmark': 'Denmark', 'Czechia': 'Czech Republic',
-                'Czech republic': 'Czech Republic', 'Company in Germany. I work from Pakistan.': 'Germany', 'croatia': 'Croatia',
-                'finland':'Finland', 'france': 'France', 'czech republic': 'Czech Republic', 'denmark': 'Denmark', 'spain': 'Spain',
-                'From Romania, but for an US based company': 'Romania', 'Austria, but I work remotely for a Dutch/British company': 'Austria',
-                'SWITZERLAND': 'Switzerland', 'switzerland': 'Switzerland', 'Luxemburg': 'Luxembourg', 'The netherlands': 'The Netherlands',
-                'ireland': 'Ireland', 'netherlands': 'The Netherlands', 'germany': 'Germany', 'Netherlands': 'The Netherlands',
-                'Nederland': 'Netherlands', 'Nederland': 'The Netherlands', 'Italy (South)': 'Italy', 'Catalonia': 'Spain',
-                'Jersey, Channel islands': 'Jersey', 'the netherlands': 'The Netherlands'}
+errage = {
+        'INDIA': 'India', 'Sri lanka': 'Sri Lanka', 'pakistan':'Pakistan', 'ARGENTINA BUT MY ORG IS IN THAILAND': 'Argentina',
+        'United States- Puerto Rico': 'Puerto Rico', 'México':'Mexico', 'Brasil': 'Brazil', 'NZ': 'New Zealand',
+        'New zealand': 'New Zealand', 'australia': 'Australia', 'Australian': 'Australia', 'New Zealand Aotearoa': 'New Zealand',
+        'Australi': 'Australia', 'Aotearoa New Zealand': 'New Zealand', 'new zealand': 'New Zealand',  'japan': 'Japan',
+        'From New Zealand but on projects across APAC': 'New Zealand', 'Remote (philippines)': 'Philippines', 'FRANCE': 'France',
+        'singapore': 'Singapore', 'UAE': 'United Arab Emirates', 'Japan, US Gov position': 'Japan','Mainland China': 'China',
+        'hong konh': 'Hong Kong', 'NIGERIA': 'Nigeria', 'South africa': 'South Africa', 'europe': 'Czech Republic',
+        'the Netherlands': 'The Netherlands', 'NL': 'The Netherlands', 'Danmark': 'Denmark', 'Czechia': 'Czech Republic',
+        'Czech republic': 'Czech Republic', 'Company in Germany. I work from Pakistan.': 'Germany', 'croatia': 'Croatia',
+        'finland':'Finland', 'france': 'France', 'czech republic': 'Czech Republic', 'denmark': 'Denmark', 'spain': 'Spain',
+        'From Romania, but for an US based company': 'Romania', 'Austria, but I work remotely for a Dutch/British company': 'Austria',
+        'SWITZERLAND': 'Switzerland', 'switzerland': 'Switzerland', 'Luxemburg': 'Luxembourg', 'The netherlands': 'The Netherlands',
+        'ireland': 'Ireland', 'netherlands': 'The Netherlands', 'germany': 'Germany', 'Netherlands': 'The Netherlands',
+        'Nederland': 'Netherlands', 'Nederland': 'The Netherlands', 'Italy (South)': 'Italy', 'Catalonia': 'Spain',
+        'Jersey, Channel islands': 'Jersey', 'the netherlands': 'The Netherlands'
+        }
 
 df['Country'] = df['Country'].replace(errage)
 
@@ -93,26 +106,53 @@ countries = [
         'Serbia', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Somalia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'Sweden',
         'Switzerland', 'Taiwan', 'Thailand', 'The Bahamas', 'The Netherlands', 'Trinidad and Tobago', 'Turkey', 'USA', 'Uganda', 'Ukraine',
         'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Vietnam'
-        ]
+]
 
 df = df[df['Country'].isin(countries)]
 
-# Shift value from Other Currency to Currency and delete the first 
-currencies = [
-    'USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD', 'ARS',
-    'NOK', 'MXN', 'SGD', 'HKD', 'KRW', 'TRY', 'RUB', 'INR', 'BRL', 'ZAR',
-    'TWD', 'DKK', 'PLN', 'THB', 'IDR', 'HUF', 'CZK', 'ILS', 'CLP', 'PHP',
-    'AED', 'COP', 'SAR', 'MYR', 'RON', 'NGN', 'EGP', 'ARS', 'PKR', 'KES',
-    'VND', 'IQD', 'CLF', 'BDT', 'DZD', 'KZT', 'QAR', 'PEN', 'UAH', 'MAD'
-]
-
-for i, currency in enumerate(df['Other Currency']):
-        if not pd.isna(currency):
-                print(i, currency)
-                df.loc[i, 'Currency'] = currency
-
-df.to_excel('clean.xlsx')
-
-
 #total_values = df['Country'].value_counts()
 #print(tabulate(total_values.reset_index().sort_values(by='Country'), headers=['Country', 'Count'], tablefmt='pretty'))
+
+# Shift value from Other Currency to Currency, delete the first and make Currency's values in Upper
+df['Currency'] = df['Currency'].astype(str)
+df['Other Currency'] = df['Other Currency'].astype(str)
+
+df = df[df['Other Currency'].str.len() <= 25] 
+
+currencies = {
+        'US Dollar': 'USD', 'BRL (R$)': 'BRL', 'RM': 'MYR', 'Canadian': 'CAD', 'croatian kuna': 'HRK', 'Euro': 'EUR', 'Polish Złoty': 'PLN',
+        'American Dollars': 'USD', 'AUD Australian': 'AUD', 'Australian Dollars': 'AUD', 'Singapore Dollara': 'SGD', 'canadian': 'CAD',
+        'RMB (chinese yuan)': 'CNY', 'Canadian': 'CAD', 'PLN (Polish zloty)': 'PLN', 'PLN (Zwoty)': 'PLN', 'NIS (new Israeli shekel)': 'ILS',
+        'Mexican Pesos': 'MXN', 'ILS/NIS': 'ILS', 'China RMB': 'CNY', 'Danish Kroner': 'DKK', 'czech crowns': 'CZK', 'Mexican pesos': 'MXN',
+        'Philippine peso (PHP)': 'PHP', 'PhP (Philippine Peso)': 'PHP', 'Israeli Shekels': 'ILS', 'Norwegian kroner (NOK)': 'NOK',
+        'Thai Baht': 'THB', 'Philippine Pesos': 'PHP', 'Taiwanese dollars': 'TWD', 'Argentine Peso': 'ARS', 'THAI  BAHT': 'THB', 'Rs': 'INR',
+        'Argentinian peso (ARS)': 'ARS', 'NTD': 'TWD', 'Peso Argentino': 'ARS', 'Philippine Peso': 'PHP', 'INR (Indian Rupee)': 'INR',
+        'Rupees': 'INR', 'Indian rupees': 'INR', 'KRW (Korean Won)': 'KRW', 'Korean Won': 'KRW'
+}
+
+df['Other Currency'] = df['Other Currency'].replace(currencies)
+df['Currency'] = df['Currency'].mask(df['Currency']=='Other', df['Other Currency'])
+df['Currency'] = df['Currency'].mask((df['Other Currency'].str.len() == 3) & (df['Other Currency']!= 'nan'), df['Other Currency'])
+df['Currency'] = df['Currency'].str.upper()
+
+df = df.drop(columns= 'Other Currency')
+elements_to_delete = ['NAN', 'N/A']
+df = df.drop(df[df['Currency'].isin(elements_to_delete)].index)
+
+# Sum of columns Salary and Additional
+df['Salary'] = df['Salary'] + df['Additional']
+df = df.drop(columns='Additional')
+
+# Standardize the Salary in USD, every pair at 01/01/2024
+GBP = 1.2723
+EUR = 1.1044
+CAD = 0.6764
+TRY = 0.0
+print(df['Currency'].unique())
+
+
+
+#df.to_excel('clean.xlsx')
+
+
+
